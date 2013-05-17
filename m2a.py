2e164d39a706d2054add9c8d9a9d2f9c10eb2b10
Michael Krisper <michael.krisper@student.tugraz.at>
2013-05-17 12:57:51 +0200

    changed usage-string to a more readable form in the sourcecode

--- m2a.py
+++ m2a.py
@@ -31,44 +31,44 @@ DEFAULT_ARCHIVE_PATH = os.environ['HOME'] + "/archive/events_memories"
 
 PAUSEONEXITTEXT = "    press <Enter> to quit"
 
-USAGE = u"\n\
-    " + sys.argv[0] + u" <options> <file(s)>\n\
-\n\
-This script moves items (files or directories) containing ISO datestamps \n\
-like \"YYYY-MM-DD\" into a directory stucture for the corresponding year.\n\
-\n\
-You define the base directory either in this script (or using the\n\
-command line argument \"--archivedir\"). The convention is e.g.:\n\
-\n\
-        <archivepath>/2009\n\
-        <archivepath>/2010\n\
-        <archivepath>/2011\n\
-\n\
-Per default, this script extracts the year from the datestamp of\n\
-each file and moves it into the corresponding directory for its year:\n\
-\n\
-     " + sys.argv[0] + u" 2010-01-01_Jan2010.txt 2011-02-02_Feb2011.txt\n\
-... moves \"2010-01-01_Jan2010.txt\" to \"<archivepath>/2010/\"\n\
-... moves \"2011-02-02_Feb2011.txt\" to \"<archivepath>/2011/\"\n\
-\n\
-OPTIONALLY you can define a sub-directory name with option \"-d DIR\". If it\n\
-contains no datestamp by itself, a datestamp from the first file of the\n\
-argument list will be used. This datestamp will be put in front of the name:\n\
-\n\
-     " + sys.argv[0] + u"  -d \"2009-02-15 bar\"  one two three\n\
-... moves all items to: \"<archivepath>/2009/2009-02-15 bar/\"\n\
-\n\
-     " + sys.argv[0] + u"  -d bar  2011-10-10_one 2008-01-02_two 2011-10-12_three\n\
-... moves all items to: \"<archivepath>/2011/2011-10-10 bar/\"\n\
-\n\
-If you feel uncomfortable you can simulate the behavior using the \"--dryrun\"\n\
-option. You see what would happen without changing anything at all.\n\
-\n\
-\n\
-:copyright: (c) 2011 by Karl Voit <tools@Karl-Voit.at>\n\
-:license: GPL v2 or any later version\n\
-:bugreports: <tools@Karl-Voit.at>\n\
-:version: "+PROG_VERSION_NUMBER+" from "+PROG_VERSION_DATE+"\n"
+USAGE = u"""
+    {0} <options> <file(s)>
+
+This script moves items (files or directories) containing ISO datestamps
+like "YYYY-MM-DD" into a directory stucture for the corresponding year.
+
+You define the base directory either in this script (or using the
+command line argument "--archivedir"). The convention is e.g.:
+
+        <archivepath>/2009
+        <archivepath>/2010
+        <archivepath>/2011
+
+Per default, this script extracts the year from the datestamp of
+each file and moves it into the corresponding directory for its year:
+
+     {0} 2010-01-01_Jan2010.txt 2011-02-02_Feb2011.txt
+... moves "2010-01-01_Jan2010.txt" to "<archivepath>/2010/"
+... moves "2011-02-02_Feb2011.txt" to "<archivepath>/2011/"
+
+OPTIONALLY you can define a sub-directory name with option "-d DIR". If it
+contains no datestamp by itself, a datestamp from the first file of the
+argument list will be used. This datestamp will be put in front of the name:
+
+     {0}  -d "2009-02-15 bar"  one two three
+... moves all items to: "<archivepath>/2009/2009-02-15 bar/"
+
+     {0}  -d bar  2011-10-10_one 2008-01-02_two 2011-10-12_three
+... moves all items to: "<archivepath>/2011/2011-10-10 bar/"
+
+If you feel uncomfortable you can simulate the behavior using the "--dryrun"
+option. You see what would happen without changing anything at all.
+
+
+:copyright: (c) 2011 by Karl Voit <tools@Karl-Voit.at>
+:license: GPL v2 or any later version
+:bugreports: <tools@Karl-Voit.at>
+:version: {1} from {2}\n""".format(sys.argv[0], PROG_VERSION_NUMBER, PROG_VERSION_DATE)
 
 parser = OptionParser(usage=USAGE)
 
